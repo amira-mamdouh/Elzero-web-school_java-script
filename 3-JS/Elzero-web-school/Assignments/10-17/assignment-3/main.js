01946788de634af860255642c35fce6695407382
Amira Mamdouh <113823054+amira-mamdouh@users.noreply.github.com>
2023-08-07 16:32:06 +0300

    Delete 3-JS/Elzero-web-school directory

--- 3-JS/Elzero-web-school/Assignments/10-17/assignment-3/main.js
+++
@@ -1,8 +0,0 @@
-// `I'm In
-// \\
-// Love \\ """ '''
-// ++ With ++
-// \"""\"""
-// ""JavaScript""``
-
-console.log("\`I'm In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n \+\+ With \+\+ \n \\ \"\"\" \\ \"\"\" \n \"\"JavaScript\"\"\`\`")
--- 3-JS/Elzero-web-school/Assignments/18-22/assignment-4/main.js
+++
@@ -1,9 +0,0 @@
-let points = 10;
-
-// Write Your Code Here
-
-console.log(++points + (+true + +true)); // 13
-
-// Write Your Code Here
-
-console.log(--points - (+true + +true)); // 8;
